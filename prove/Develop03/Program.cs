c50eca71119146a389faeef40463435dae9ae0c3
niwemu1 <niwemugenideborah@gmail.com>
2024-05-25 13:08:40 -0700

    Completed prove Develop03

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -1,9 +1,18 @@
 using System;
 
+
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop03 World!");
+        Scripture myScripture = new Scripture();
+        myScripture.GetDisplayText();
+        Console.WriteLine(myScripture.GetDisplayText());
+
+        Reference myReference = new Reference();
+        myReference.GetDisplayText();
+        Console.WriteLine(myReference.GetDisplayText());
+
+
     }
 }
